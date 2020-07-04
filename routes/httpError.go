d638c5df347e18b69407fc6fd3cfcaa83f4bf750
S. B. Immington <snirk.immington@gmail.com>
2020-07-04 10:37:44 -0400

    Switch errors over to panics

---
+++ routes/httpError.go
@@ -0,0 +1,79 @@
+package routes
+
+import (
+	"net/http"
+)
+
+var abortedRequestPanicMessage string
+
+func abortRequest() {
+	panic(abortedRequestPanicMessage)
+}
+
+func httpBadRequestIf(response Response, request *Request, err error) {
+	if err != nil {
+		logf(request, "-> 400 Bad Request: %v", err)
+		http.Error(response, "Bad Request", http.StatusBadRequest)
+		abortRequest()
+	}
+}
+
+func httpBadRequest(response Response, request *Request, message string) {
+	if message == "" {
+		message = "Bad Request"
+	}
+	logf(request, "-> 400 %s", message)
+	http.Error(response, message, http.StatusBadRequest)
+	abortRequest()
+}
+
+func httpNotFoundIf(response Response, request *Request, err error) {
+	if err != nil {
+		logf(request, "-> 404 Not Found: %v", err)
+		http.Error(response, "Not Found", http.StatusNotFound)
+		abortRequest()
+	}
+}
+
+func httpNotFound(response Response, request *Request, message string) {
+	if message == "" {
+		message = "Not Found"
+	}
+	logf(request, "-> 404 %s", message)
+	http.Error(response, message, http.StatusNotFound)
+	abortRequest()
+}
+
+func httpUnauthorizedIf(response Response, request *Request, err error) {
+	if err != nil {
+		logf(request, "-> 401 Unauthorized: %v", err)
+		http.Error(response, "Unauthorized", http.StatusUnauthorized)
+		abortRequest()
+	}
+}
+
+func httpUnauthorized(response Response, request *Request, message string) {
+	if message == "" {
+		message = "Unauthorized"
+	}
+	logf(request, "-> 401 %s", message)
+	http.Error(response, message, http.StatusUnauthorized)
+	abortRequest()
+}
+
+func httpInternalErrorIf(response Response, request *Request, err error) {
+	if err != nil {
+		logf(request, "-> 500 Internal Server Error: %v", err)
+		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
+		abortRequest()
+	}
+}
+
+func httpInternalError(response Response, request *Request, message string) {
+	if message == "" {
+		message = "Internal Server Error"
+	}
+	logf(request, "-> 500 %s", message)
+	http.Error(response, message, http.StatusInternalServerError)
+	abortRequest()
+}
--- routes/request.go
+++ routes/request.go
@@ -1,4 +1,3 @@
-// Request middleware(s) for all requests.
 package routes
 
 import (
@@ -7,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"sr/config"
-	"time"
 )
 
 type Request = http.Request
@@ -42,59 +40,6 @@ func logf(request *Request, format string, values ...interface{}) {
 	if err != nil {
 		log.Printf(id+" [Output Error] "+format, values...)
 	}
-}
-
-func httpNotFound(response Response, request *Request) bool {
-	logf(request, "-> 404 not found")
-	http.Error(response, "Not found", http.StatusNotFound)
-	return true
-}
-
-func httpUnauthorized(response Response, request *Request, err error) bool {
-	logf(request, "-> 401 Unauthorized: %v", err)
-	http.Error(response, "Unauthorized", http.StatusUnauthorized)
-	return true
-}
-
-func httpInternalError(response Response, request *Request, err error) bool {
-	if config.IsProduction {
-		logf(request, "Internal error handling %v %v %v %v: %v",
-			request.RemoteAddr, request.Proto, request.Method, request.URL, err,
-		)
-		logf(request, "-> 500 Internal Error")
-		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
-	} else {
-		message := fmt.Sprintf("Internal error handling %v %v: %v",
-			request.Method, request.URL, err,
-		)
-		logf(request, message)
-		logf(request, " -> 500 Internal error: %v", err)
-		http.Error(response, message, http.StatusInternalServerError)
-	}
-	return true
-}
-
-func httpInternalErrorMessage(response Response, request *Request, message interface{}) bool {
-	if config.IsProduction {
-		logf(request, "Internal error handling %v %v %v %v: %v",
-			request.RemoteAddr, request.Proto, request.Method, request.URL, message,
-		)
-		logf(request, "-> 500 Internal Server Error")
-		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
-	} else {
-		logMessage := fmt.Sprintf("Internal error handling %v %v %v: %v",
-			request.Method, request.URL, message,
-		)
-		logf(request, "-> 500 %v", logMessage)
-		http.Error(response, logMessage, http.StatusInternalServerError)
-	}
-	return true
-}
-
-func httpInvalidRequest(response Response, request *Request, message string) {
-	logf(request, "-> 400 Bad Request: ", message)
-	http.Error(response, message, http.StatusBadRequest)
-	return true
 }
 
 func httpSuccess(response Response, request *Request, message ...interface{}) {
