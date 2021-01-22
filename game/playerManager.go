9bf061dd59aee8fadab4b760827377b8c3004207
S. B. Immington <shadowroller@immington.industries>
2021-01-22 16:46:28 -0500

    Add function to update player online status

--- game/playerManager.go
+++ game/playerManager.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gomodule/redigo/redis"
+	"log"
 	"sr/id"
 	"sr/player"
 	"sr/update"
@@ -39,6 +40,40 @@ func AddPlayer(gameID string, player *player.Player, conn redis.Conn) error {
 		)
 	}
 	return nil
+}
+
+func UpdatePlayerConnections(gameID string, playerID id.UID, mod int, conn redis.Conn) (int, error) {
+	newConns, err := player.ModifyConnections(playerID, mod, conn)
+	if err != nil {
+		return 0, fmt.Errorf("redis error updating connections for %v: %w", playerID, err)
+	}
+	// Race conditions
+	// - We assume that for each incr we will eventually send a decr.
+	// - We assume we have not sent a decr without having sent an incr.
+	// - Redis incr and decr are atomic, so we assume the sends will go through.
+	// - We DO NOT assume the sends will go through in order.
+	// - We assume the connection MAY drop below 0 if decrs are processed before incrs.
+	// We assume this may only happen in a sceario where an incrs will come through later
+	// and match the decrs; i.e. if we have observed connections < 0 we expect to settle on
+	// connections = 0. Because of this, we do not send updates for connections < 0.
+	var ud update.Player
+	// Decreasd to 0; going offline
+	if newConns == 0 && mod == player.DecreaseConnections {
+		ud = update.ForPlayerOnline(playerID, false)
+	} else if newConns == 1 && mod == player.IncreaseConnections {
+		ud = update.ForPlayerOnline(playerID, true)
+	} else {
+		log.Printf("Mod = %v, connections = %v, not sending an update", mod, newConns)
+		return newConns, nil
+	}
+	updateBytes, err := json.Marshal(ud)
+	if err != nil {
+		return newConns, fmt.Errorf("unable to marshal %#v to JSON: %w", ud, err)
+	}
+	if _, err = conn.Do("PUBLISH", "update:"+gameID, updateBytes); err != nil {
+		return newConns, fmt.Errorf("redis error publishing %#v: %w", ud, err)
+	}
+	return newConns, nil
 }
 
 // UpdatePlayer updates a player in the database.
