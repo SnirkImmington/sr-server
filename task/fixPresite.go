5ad59755f7c844744dbd4bb57b9591bd0aa0fda9
S. B. Immington <shadowroller@immington.industries>
2021-03-18 20:15:58 -0400

    Add script to hack script tags back into presite build file

---
+++ task/fixPresite.go
@@ -0,0 +1,60 @@
+package task
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// Closing content of the prerendered file. presite doesn't include a trailing newline!
+const closingContent = `</body></html>`
+
+// Yep, using a regex to find an HTML tag
+var matchScriptSrc = regexp.MustCompile(`<script src="(./static/js/[^j]*js)">`)
+
+func handleRewritePresiteIndexTask(buildIndexPath string, presiteIndexPath string) error {
+	baseFile, err := os.Open(buildIndexPath)
+	if err != nil {
+		return fmt.Errorf("could not open base file %s: %w", buildIndexPath, err)
+	}
+	defer baseFile.Close()
+	baseFileBytes, err := io.ReadAll(baseFile)
+	if err != nil {
+		return fmt.Errorf("could not read all from base file %s: %w", buildIndexPath, err)
+	}
+	log.Printf("Read build file %s", buildIndexPath)
+
+	matches := matchScriptSrc.FindAll(baseFileBytes, -1)
+	var result strings.Builder
+	for _, match := range matches {
+		log.Printf("Found `%s`", match)
+		result.Write(match)             // whole <script> tag
+		result.WriteString("</script>") // and closing tag
+	}
+
+	presiteFile, err := os.OpenFile(presiteIndexPath, os.O_RDWR, 0o755)
+	if err != nil {
+		return fmt.Errorf("could not open presite file %s: %w", presiteIndexPath, err)
+	}
+	defer presiteFile.Close()
+	log.Printf("Opened presite file %s", presiteIndexPath)
+	info, err := presiteFile.Stat()
+	if err != nil {
+		return fmt.Errorf("could not stat prestie file %s: %w", presiteIndexPath, err)
+	}
+	offset := info.Size() - int64(len(closingContent))
+	if _, err = presiteFile.Seek(offset, 0); err != nil {
+		return fmt.Errorf("could not seek to offset %v (of %v) in %s: %w", offset, info.Size(), presiteIndexPath, err)
+	}
+
+	if _, err = presiteFile.WriteString(result.String()); err != nil {
+		return fmt.Errorf("could not write to presite file %s: %w", presiteIndexPath, err)
+	}
+	if _, err = presiteFile.WriteString(closingContent); err != nil {
+		return fmt.Errorf("could not write closing to presite file %s: %w", presiteIndexPath, err)
+	}
+	return nil
+}
