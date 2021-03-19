57999fce98f6582e1307c9aa44d4c00c4072f33e
S. B. Immington <shadowroller@immington.industries>
2021-03-18 22:09:42 -0400

    Use hardware random for all rolls

--- roll.go
+++ roll.go
@@ -4,8 +4,10 @@ import (
 	crypto "crypto/rand"
 	"encoding/binary"
 	"log"
+	"math"
 	"math/rand"
 	"sr/config"
+	"time"
 )
 
 // RerollTypeRerollFailures represents the "Reroll Failures" use of post-roll edge.
@@ -90,6 +92,72 @@ func BeginGeneratingRolls() {
 			rng := rand.New(rand.NewSource(seed))
 			for i := 0; i < config.RollBufferSize; i++ {
 				rollsChan <- rng.Intn(6) + 1
+			}
+		}
+	}()
+}
+
+const rollMax = 6 // RNG uses modulo, anything % 6 is gonna be in { 0, 1, 2, 3, 4, 5 }
+const inputByteMax = math.MaxUint8 - ((math.MaxUint8 % rollMax) + 1)
+
+// A note on RNG here:
+//
+// The crypto API doesn't include a "range" version of the RNG for regular ints.
+// It dos have a .Int(max) *big.Int. That seemed like overkill for rolling 1-6,
+// so I took a look. This algorithm is based on that implementation and I believe
+// it is sound and simple enough to understand.
+//
+// We assume that crypto.Read will produce random bytes. We assume that each byte
+// has a uniform distribution of values, 0 ... 255. We assume the value of each
+// byte is independant of the value of any other byte.
+//
+// We could partition this possible output set via modulo operator %. If we look
+// at the output % 8, we can get 8 values: 0, 1, 2, 3, 4, 5, 6, 7.
+// For each of thse outputs, there are exactly 32 values in the input set which
+// produce the output. The random byte is equally likely to be any of the 8
+// answers.
+//
+// Our dice rolls do not work with this premise as there not the same number of
+// input bytes for each output roll. Taking a byte % 6 is more likely to produce
+// vaues 0, 1, 2, 3 than 4 and 5. Thus, we choose to partition the input futher.
+//
+// We choose our maximum desired value inputByteMax as being closest to the byte
+// max while still having equal number of members which produce th desired rolls.
+// Bytes <= inputByteMax (251) have 42 members which each map to a given roll,
+// satisfying the condition for 8 above.
+//
+// We are able to get random values <= inputByteMax by generating random bytes
+// and discarding bytes > inputByteMax. Because we assume independant events, each
+// discard does not affect the value of other bytes. Because we assume each value
+// 0 .. 255 has a uniform 1/256 chance of appearing in a byte, we should assume
+// each value 0 .. inputByteMax has a 1/inputByteMax+1 chance of appearing in a
+// byte which is <= inputByteMax.
+
+func GenerateRolls() {
+	rollsChan = make(chan int, config.RollBufferSize)
+	go func() {
+		// There's some amount of caching behind the scenes, but it didn't
+		// feel right to grab 1 byte at a time from the RNG.
+		// As long as the buffer is less than maybe a megabyte, it should be
+		// fine. (The max limit is around 1 << 25 bytes from RNG sources.)
+		// Even if hardware RNG goes dark for some time, we can still do
+		// all the rolls in potentially two buffers. Ultimately, you can't _fix_
+		// a throughput issue with more buffering.
+		bytes := make([]uint8, config.RollBufferSize)
+		for {
+			_, err := crypto.Read(bytes)
+			// Read will either read to full or report an error.
+			if err != nil {
+				log.Printf("Error calling roll RNG: %v", err)
+				time.Sleep(time.Duration(5) * time.Millisecond)
+				continue
+			}
+			for _, randByte := range bytes {
+				if randByte <= inputByteMax {
+					// convert 0 .. 5 (result of % 6) to 1 .. 6
+					rollsChan <- int((randByte % rollMax) + 1)
+				}
+				// Just skip bytes between inputByteMax and 255;
 			}
 		}
 	}()
