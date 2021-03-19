1bbdab0302003fa0319819513829944fcab22df7
S. B. Immington <shadowroller@immington.industries>
2021-03-18 20:25:03 -0400

    Fix presite task when not using relative paths

--- task/fixPresite.go
+++ task/fixPresite.go
@@ -13,7 +13,7 @@ import (
 const closingContent = `</body></html>`
 
 // Yep, using a regex to find an HTML tag
-var matchScriptSrc = regexp.MustCompile(`<script src="(./static/js/[^j]*js)">`)
+var matchScriptSrc = regexp.MustCompile(`<script src="(/static/js/[^j]*js)">`)
 
 func handleRewritePresiteIndexTask(buildIndexPath string, presiteIndexPath string) error {
 	baseFile, err := os.Open(buildIndexPath)
@@ -28,6 +28,9 @@ func handleRewritePresiteIndexTask(buildIndexPath string, presiteIndexPath strin
 	log.Printf("Read build file %s", buildIndexPath)
 
 	matches := matchScriptSrc.FindAll(baseFileBytes, -1)
+	if len(matches) < 3 {
+		return fmt.Errorf("Expected to get at least 3 script tags, got %v", matches)
+	}
 	var result strings.Builder
 	for _, match := range matches {
 		log.Printf("Found `%s`", match)
@@ -49,10 +52,12 @@ func handleRewritePresiteIndexTask(buildIndexPath string, presiteIndexPath strin
 	if _, err = presiteFile.Seek(offset, 0); err != nil {
 		return fmt.Errorf("could not seek to offset %v (of %v) in %s: %w", offset, info.Size(), presiteIndexPath, err)
 	}
+	log.Printf("Seeked to end of file")
 
 	if _, err = presiteFile.WriteString(result.String()); err != nil {
 		return fmt.Errorf("could not write to presite file %s: %w", presiteIndexPath, err)
 	}
+	log.Printf("Wrote all the tags in")
 	if _, err = presiteFile.WriteString(closingContent); err != nil {
 		return fmt.Errorf("could not write closing to presite file %s: %w", presiteIndexPath, err)
 	}
